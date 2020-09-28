acd09288b34bcbbb1f9f84e002626984b6960979
sophie rouchon <sophie.rouchon@gmail.com>
2020-09-28 11:10:34 +0200

    create validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   belongs_to :company
+  validates :name, presence :true
+  validates :address, presence :true
+  validates :postcode, presence :true
+  validates :city, presence :true
+  validates :phone_1, presence :true
+  validates :email_1, presence :true
+  validates :phone_2, presence :true
+  validates :email_2, presence :true
+  validates :siret, presence :true
 end
