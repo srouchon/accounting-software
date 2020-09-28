7fad666aeb0c8bb083a1ffecbcad0beb2c07279d
sophie rouchon <sophie.rouchon@gmail.com>
2020-09-28 10:58:19 +0200

    create models and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_081905) do
+ActiveRecord::Schema.define(version: 2020_09_28_085412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "postcode"
+    t.string "city"
+    t.string "phone"
+    t.string "email"
+    t.string "siret"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "ref_client"
+    t.string "name"
+    t.string "address"
+    t.integer "postcode"
+    t.string "city"
+    t.string "phone_1"
+    t.string "phone_2"
+    t.string "email_1"
+    t.string "email_2"
+    t.string "siren"
+    t.string "siret"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_customers_on_company_id"
+  end
+
+  create_table "user_companies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_user_companies_on_company_id"
+    t.index ["user_id"], name: "index_user_companies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +62,14 @@ ActiveRecord::Schema.define(version: 2020_09_28_081905) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "customers", "companies"
+  add_foreign_key "user_companies", "companies"
+  add_foreign_key "user_companies", "users"
 end
