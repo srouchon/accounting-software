baf32d6349c3f4d8c98620013c1e0417d1d38af0
sophie rouchon <sophie.rouchon@gmail.com>
2020-10-05 17:10:17 +0200

    start customers controller private methods strong params + set_customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
   def index
     
   end
@@ -10,16 +12,30 @@ class CustomersController < ApplicationController
   def new
     
   end
+  
   def create
     
   end
+  
   def edit
     
   end
+  
   def update
     
   end
+  
   def destroy
     
+  end
+  
+  private
+  
+  def set_customer
+    @customer = customer.find(params[:id])
+  end
+  
+  def customer_params
+    params.require(:customer).permit(:ref_client, :name, :address, :postcode, :city, :phone1, :email1, :phone2, :email2, :siren, :siret)
   end
 end
