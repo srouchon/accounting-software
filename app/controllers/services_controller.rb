be8849fd2f1bb8f61bfe461316e50cac5d4764e5
sophie rouchon <sophie.rouchon@gmail.com>
2020-10-28 16:07:12 +0100

    change routes for services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,19 +1,20 @@
 class ServicesController < ApplicationController
   before_action :set_company, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  before_action :set_customer, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  before_action :set_quote, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  
+
   def index
     @services = policy_scope(Service)
   end
-  
+
+  def show
+  end
+
   def new
     @service = Service.new
     @quoteservice = QuoteService.new
     authorize @service
     authorize @quoteservice
   end
-  
+
   def create
     @service = Service.new(ref_service: service_params[:ref_service], description_service: service_params[:description_service], unit_price: service_params[:unit_price])
     authorize @service
@@ -25,35 +26,24 @@ class ServicesController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
-    
   end
-  
+
   def updates
-    
   end
-  
+
   def destroy
-    
   end
-  
+
   private
-  
+
   def set_company
     @company = Company.find(params[:company_id])
   end
-  
-  def set_customer
-    @customer = Customer.find(params[:customer_id])
-  end
-  
-  def set_quote
-    @quote = Quote.find(params[:quote_id])
-  end
-  
+
   def service_params
     params.require(:service).permit(:description_service, :ref_service, :unit_price, :quantity, :total_price_service)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :companies do
     resources :customers do
-      resources :quotes do
-        resources :services, only: [:new, :create, :edit, :update, :destroy]
-      end
+      resources :quotes
       resources :bills
     end
-    resources :services, only: [:index]
+    resources :services
   end
 end
