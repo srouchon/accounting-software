c2ede2b5bf169551596c1b8850b4beb113a66c0b
sophie rouchon <sophie.rouchon@gmail.com>
2020-11-12 11:30:12 +0100

    display filtered quotes to customer_id

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,7 +4,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = policy_scope(Quote)
+    @quotes = policy_scope(Quote).where(customer_id: @customer)
   end
 
   def show
