ad103ce6bb5ce0ab7b6a433bb8d6195987cf7d32
sophie rouchon <sophie.rouchon@gmail.com>
2020-11-19 11:43:20 +0100

    add classes to input fields

--- app/javascript/channels/form.js
+++ app/javascript/channels/form.js
@@ -1,3 +1,4 @@
+// Create button that click on two buttons same time
 const btnAddService = document.getElementById('btn_add_service');
 const btnAddQuoteService = document.getElementById('btn_add_quote_service');
 const btnNewLine = document.getElementById('btn_new_line');
@@ -8,8 +9,8 @@ function clickBtn() {
       event.preventDefault();
       btnAddService.click();
       btnAddQuoteService.click();
-    }
-  )
-}};
+    });
+  }
+};
 
 export {clickBtn};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,11 +28,8 @@ require("@nathanvda/cocoon")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
 import { clickBtn } from '../channels/form.js';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
   clickBtn();
 });
