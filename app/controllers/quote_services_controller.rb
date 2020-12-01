3fdca1243495f74d269800e0452ff86a9df5b3f9
sophie rouchon <sophie.rouchon@gmail.com>
2020-12-01 11:16:54 +0100

    implement quoteservices#destroy

--- app/controllers/quote_services_controller.rb
+++ app/controllers/quote_services_controller.rb
@@ -1,28 +1,47 @@
 class QuoteServicesController < ApplicationController
-  
+  before_action :set_company, only: [:create, :destroy]
+  before_action :set_customer, only: [:create, :destroy]
+  before_action :set_quote, only: [:create, :destroy]
+
   def new
     @quote_service = QuoteService.new
   end
   
   def create
-    company = params[:company_id]
-    customer = params[:customer_id]
     service = Service.find(params[:service][:service_id])
-    quote = Quote.find(params[:quote_id])
     quote_service = QuoteService.new(quote_services_params)
     quote_service.service = service
-    quote_service.quote = quote
+    quote_service.quote = @quote
     quote_service.total_price_service = service.unit_price * quote_service.quantity
     authorize quote_service
     if quote_service.save!
-      redirect_to company_customer_quote_path(company, customer, quote)
+      redirect_to company_customer_quote_path(@company, @customer, @quote)
     else
       render :new
     end
   end
   
+  def destroy
+    quote_service = QuoteService.find(params[:id])
+    quote_service.destroy
+    authorize quote_service
+    redirect_to company_customer_quote_path(@company, @customer, @quote)
+  end
+  
   private
   
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:customer_id])
+  end
+  
+  def set_quote
+    @quote = Quote.find(params[:quote_id])
+  end
+
   def quote_services_params
     params.require(:quote_service).permit(:quantity)
   end
