7fe12859e572fe4ca9c009521f89585458e72cfe
sophie rouchon <sophie.rouchon@gmail.com>
2020-12-02 11:42:51 +0100

    remove useless joins causing problems

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,5 @@
 class Bill < ApplicationRecord
   belongs_to :customer
-  belongs_to :company
   has_many :bill_services, dependent: :destroy
   has_many :services, through: :bill_services
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,5 @@
 class Quote < ApplicationRecord
   belongs_to :customer
-  belongs_to :company
   has_many :quote_services, dependent: :destroy
   has_many :services, through: :quote_services
 end
