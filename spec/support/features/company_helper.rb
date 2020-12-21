8ec8990f0ff94bb158357ec88f762719327ae58c
sophie rouchon <sophie.rouchon@gmail.com>
2020-12-21 16:45:16 +0100

    implement test when user modifies company from companies#index

--- spec/support/features/company_helper.rb
+++ spec/support/features/company_helper.rb
@@ -15,4 +15,19 @@ module Features
     fill_in("TVA",	with: tva)
     click_on('Créer cette entreprise')
   end
+  
+  def modify_company(name, address, postcode, city, phone, email, siret, tva)
+    find('.edit').click
+    expect(page).to have_css('h1', text: "Modifier les informations du l'entreprise")
+    fill_in("Dénomination sociale",	with: name)
+    fill_in("Adresse",	with: address)
+    fill_in("Code postal",	with: postcode)
+    fill_in("Ville",	with: city)
+    fill_in("Téléphone",	with: phone)
+    fill_in("Email",	with: email)
+    fill_in("SIRET",	with: siret)
+    fill_in("TVA",	with: tva)
+    click_on('Modifier cette entreprise')
+  end
+  
 end
